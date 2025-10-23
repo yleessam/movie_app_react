30c3278ed99341c1d0eb8a4a0e2bcab1ba56f1b1
yleekim <youngleeSsam.barnett@gmail.com>
2025-10-23 15:35:31 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component{
+  state={
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount(){
+    //영화데이터 로딩...
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    setTimeout(() => {
+      this.setState( {isLoading: false});
+    }, 6000);
+  }
+  render(){
+    const {isLoading} = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'} </div>;
+  }
 }
-
 export default App;
